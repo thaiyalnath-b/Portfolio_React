79c70dddbfa2fc0293e101bc12a5e67a77494176
ThaiyalnathB <thaiyalnath2005@gmail.com>
2026-02-17 15:13:08 +0530

    react portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Navbar from './components/Navbar/Navbar';
-import Home from "./sections/Home";
-import About from "./sections/About";
-import Skills from "./sections/Skills";
-import Projects from "./sections/Projects";
-import Contact from "./sections/Contact";
+import Home from "./sections/Home/Home";
+import About from "./sections/About/About";
+import Skills from "./sections/Skills/Skills";
+import Projects from "./sections/Projects/Projects";
+import Contact from "./sections/Contact/Contact";
 
 function App() {
   return (
--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+import profileImg from '../../assets/pro.png';
+import './Home.css';
+
 
 function Home() {
+
+  const [showToast, setShowToast] = useState(false);
+
   const downloadCV = () => {
-    window.open('src/assets/resume/resume.pdf', '_blank');
+    setShowToast(true);
+
+    const link = document.createElement('a');
+    link.href = '/resume/resume.pdf';
+    link.download = 'Thaiyalnath_Resume.pdf'; 
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    setTimeout(() => {
+      setShowToast(false);
+    }, 3000);
+
   };
 
   return (
     <section id='home' className='home-section'>
+
+      {showToast && (
+        <div className='toast-notification'>
+          <div className='toast-icon'>✓</div>
+            <span>Downloaded...</span>
+        </div>
+      )}
+
       <div className='container home-container'>
         <div className='home-content'>
           <div className='reveal-box'>
@@ -44,7 +70,7 @@ function Home() {
         <div className='home-image-wrapper'>
           <div className='blob-shape'></div>
           <div className='image-glow-backdrop'></div>
-          <img src="src/assets/pro.png" alt="Profile" className='profile-image' />
+          <img src={profileImg} alt="Profile" className='profile-image' />
         </div>
       </div>
     </section>
