dfcc27963cb1bd5b3cc0b04b96984115a665d233
ThaiyalnathB <thaiyalnath2005@gmail.com>
2026-02-26 14:33:46 +0530

    Toogle Button Fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Collapse from 'bootstrap/js/dist/collapse';
 import './Navbar.css'
 
 function Navbar() {
@@ -11,6 +12,18 @@ function Navbar() {
         window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
+    const closeMenu = () => {
+        const navbar = document.getElementById("navbarNav");
+        if (!navbar) return;
+
+        let bsCollapse = Collapse.getInstance(navbar);
+
+        if (!bsCollapse) {
+            bsCollapse = new Collapse(navbar, { toggle: false });
+        }
+
+        bsCollapse.hide();
+    };
 
     return (
         <nav className={`navbar navbar-expand-md fixed-top navbar-style ${isScrolled ? 'navbar-scrolled' : ''}`}>
@@ -32,7 +45,7 @@ function Navbar() {
                     <ul className="navbar-nav ms-auto">
                         {['home', 'about', 'skills', 'projects', 'contact'].map((item) => (
                             <li className="nav-item" key={item}>
-                                <a className="nav-link nav-custom" href={`#${item}`}>
+                                <a className="nav-link nav-custom" href={`#${item}`} onClick={closeMenu}>
                                     {item.charAt(0).toUpperCase() + item.slice(1)}
                                 </a>
                             </li>
